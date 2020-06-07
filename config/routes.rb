add2f7ca42c8c29e05a48da128099e319f0a0a99
tangent500 <tangent500@yahoo.co.jp>
2020-06-07 01:29:36 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'microposts/create'
   get 'microposts/destroy'
   get 'sessions/new'
@@ -11,5 +13,13 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
